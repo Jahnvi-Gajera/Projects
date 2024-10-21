469121f1a0866d0d1af1a59868a40033271e4ec4
Jahnvi-Gajera <155662998+Jahnvi-Gajera@users.noreply.github.com>
2024-10-21 10:25:50 +0000

    1

--- 1_color_chnage/chaiaurcode.js
+++ 1_color_chnage/chaiaurcode.js
@@ -1,2 +1,10 @@
 const buttons = document.querySelectorAll('.button')
-console.log(buttons)
+const body = document.querySelector("body")
+
+buttons.forEach(function(bt){
+    console.log(bt);
+    bt.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+    });
+});
